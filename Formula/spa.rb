302cf8b56c3da98e532704340178311854c9c8cc
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-18 20:50:57 +0900

    Brew formula update for spa version v0.0.2

--- Formula/spa.rb
+++ Formula/spa.rb
@@ -2,16 +2,16 @@
 class Spa < Formula
   desc "Spinnaker Admin tools"
   homepage "https://github.com/kekeniker/spa"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.com/kekeniker/spa/releases/v0.0.1/spa_0.0.1_darwin_amd64.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "f17534681c198cdb8d0434332a836314219343940b6b60bd183ecdfc7f780d9e"
+    url "http://github.com/kekeniker/spa/releases/v0.0.2/spa_0.0.2_darwin_amd64.tar.gz"
+    sha256 "a3b4a0c92545d4d84c48eed22b10f82ab25edb74fdd1c5352118733ef3c8a975"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "http://github.com/kekeniker/spa/releases/v0.0.1/spa_0.0.1_linux_amd64.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "ed61c66802ebd7d38f93b193caee0dabffc9abd3c9c5f1a9589606292c377e78"
+    url "http://github.com/kekeniker/spa/releases/v0.0.2/spa_0.0.2_linux_amd64.tar.gz"
+    sha256 "cab5c28e83f8a59035fd79ec005ac93b60cbcc0270ced49880896d660b4af4d2"
   end
 
   def install
